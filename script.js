6d43902e96d3369c12d29ca7bebc7cec57589caf
I1Kazma <karmakazmenko@gmail.com>
2023-01-25 20:58:56 +0300

    LevelUp v.1

---
+++ script.js
@@ -0,0 +1,70 @@
+/*анимация*/
+const animItems = document.querySelectorAll(`._anim-items`)
+if (animItems.length > 0) {
+    window.addEventListener(`scroll`, animOnScroll)
+
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffSet = offset(animItem).top
+            const animStart = 6;
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart
+            }
+            if ((pageYOffset > animItemOffSet - animItemPoint) && pageYOffset < (animItemOffSet + animItemHeight)) {
+                animItem.classList.add(`_active`)
+            } else {
+                if (!(animItem.classList.contains(`_anim-no-hide`))) {
+                    animItem.classList.remove(`_active`)
+                }
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect()
+        let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
+
+
+
+
+
+/*таймер*/
+
+document.getElementById('timer').innerHTML =
+    30 + ":" + 00;
+startTimer();
+
+
+function startTimer() {
+    var presentTime = document.getElementById('timer').innerHTML;
+    var timeArray = presentTime.split(/[:]+/);
+    var m = timeArray[0];
+    var s = checkSecond((timeArray[1] - 1));
+    if (s == 59) { m = m - 1 }
+    if (m < 0) {
+        return
+    }
+
+    document.getElementById('timer').innerHTML =
+        m + ":" + s;
+    console.log(m)
+    setTimeout(startTimer, 1000);
+
+}
+
+function checkSecond(sec) {
+    if (sec < 10 && sec >= 0) { sec = "0" + sec }; // add zero in front of numbers < 10
+    if (sec < 0) { sec = "59" };
+    return sec;
+}
